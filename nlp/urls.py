dfb623c54cf330696a112b689b68d6a168e39c23
Giannis Daras <daras.giannhs@gmail.com>
2018-08-01 20:43:39 +0300

    Add url for DEP visualization

--- nlp/urls.py
+++ nlp/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^about$', views.about, name='nlp.views.about'),
     url(r'^api/analyze$', views.analyze, name='nlp.views.analyze'),
+    url(r'^visualize$', views.visualize_view, name='nlp.views.visualize_view')
 ]
-
